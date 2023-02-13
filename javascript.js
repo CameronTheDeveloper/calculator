e566aa6a46c1f4bbb9d3d16beb886d5f84eb94eb
CameronTheDeveloper <cameronposey@outlook.com>
2023-02-12 22:46:17 -0800

    Edit style.css, add operate function in javascript.js
    The operate function takes 2 numbers and an operator to perform a mathematical operation on the two numbers. The style.css changes edit the calculator formatting.

--- javascript.js
+++ javascript.js
@@ -13,3 +13,22 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 }
+
+function operate(num1, op, num2){
+    switch (op){
+        case '+':
+            return num1 + num2;
+            break;
+        case '-':
+            return num1 - num2;
+            break;
+        case '*':
+        return num1 * num2;
+        break;
+        case '/':
+            return num1 / num2;
+            break;
+        default:
+            return "Invalid operator";
+    }
+}
